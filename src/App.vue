1557fa747d76d295c9abe0a2d02c1f75a753f09a
jarbun <jarbun306@gmail.com>
2019-03-13 11:33:52 +0530

    Added global style for buttons and removed  global styles for some elements

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,31 @@
 </template>
 
 <style lang="scss">
+  @import 'assets/master';
+
   *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-weight: normal;
-    font-size: 16px;
     list-style: none;
     font-family: 'Roboto', -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  p, h1, h2, h3, h4, h5, h6{
+    font-weight: normal;
+    font-size: 16px;
+  }
+
+  button{
+    background: none;
+    background-color: none;
+    border: none;
+    cursor: pointer;
+    text-transform: uppercase;
+    letter-spacing: 1.25px;
+    font-size: 14px;
+    font-weight: $medium;
+    padding: 10px 16px;
+    outline: none;
   }
 </style>
