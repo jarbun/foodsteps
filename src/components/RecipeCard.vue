27fd51a6e4dc248dabcb1c864cfeb6d39e2668f7
jarbun <jarbun306@gmail.com>
2019-03-15 16:43:55 +0530

    Fixed id conflict issue

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,11 +1,11 @@
 <template>
-  <router-link tag="div" :to="recipeUrl" id="recipe-card">
+  <router-link tag="div" :to="recipeUrl" class="recipe-card">
     <img :src="imageUrl" alt="recipe image">
-    <div id="content">
+    <div class="content">
       <h1>{{ title }}</h1>
-      <h2 id="creator">{{ creator }}</h2>
-      <div id="extra">
-        <span id="likes">
+      <h2 class="creator">{{ creator }}</h2>
+      <div class="extra">
+        <span class="likes">
           <span>{{ nLikes }}</span>
           <span class="material-icons">favorite</span>
         </span>
@@ -45,7 +45,7 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/master';
 
-#recipe-card{
+.recipe-card{
   display: flex;
   margin-top: 15px;
   margin-left: 10px;
@@ -60,20 +60,20 @@ img{
   box-shadow: 0 3px 6px $gray16;
 }
 
-#content{
+.content{
   display: flex;
   flex-direction: column;
   width: 100%;
   margin: 5px 20px 5px 10px;
 }
 
-#creator{
+.creator{
   font-size: 13px;
   color: $gray60;
   letter-spacing: 0.4px;
 }
 
-#extra{
+.extra{
   font-weight: $medium;
   font-size: 14px;
   color: $gray60;
@@ -82,7 +82,7 @@ img{
   margin-top: auto;
 }
 
-#likes{
+.likes{
   display: flex;
   .material-icons{
     font-size: 14px;
