ba04cecc6718d2c0912b3ab1402c84eb8dc6977b
jarbun <jarbun306@gmail.com>
2019-03-15 15:48:48 +0530

    Added basic styling for Directions

--- src/components/Recipe/Directions.vue
+++ src/components/Recipe/Directions.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <div v-for="(direction, i) in directions" :key="i">
-      {{ i+1 }} - {{ direction }}
+  <div id="main">
+    <div v-for="(direction, i) in directions" :key="i" class="direction">
+      <span class="circle">
+        {{ i + 1 }}
+      </span>
+      <span>{{ direction }}</span>
     </div>
   </div>
 </template>
@@ -14,5 +17,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/master';
 
+#main{
+  margin: 5px 0;
+}
+
+.direction{
+  display: flex;
+  padding: 10px 0;
+  margin: 0 20px 0 10px;
+}
+
+.circle{
+  min-width: 24px;
+  width: 24px;
+  height: 24px;
+  border: 1px solid $light-gray;
+  border-radius: 100%;
+  line-height: 24px;
+  text-align: center;
+  align-self: center;
+  font-size: 14px;
+  color: $gray60;
+  margin-right: 10px;
+}
 </style>
