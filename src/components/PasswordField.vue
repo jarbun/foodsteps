fddefc7658f8b515c475666e12c9e9e7929e606e
jarbun <jarbun306@gmail.com>
2019-03-16 15:12:37 +0530

    Added PasswordField component

---
+++ src/components/PasswordField.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="password-field">
+    <input :type="inputType" placeholder="Password">
+    <span class="material-icons" @click="toggleType">remove_red_eye</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'passwordfield',
+  data () {
+    return {
+      inputType: 'password'
+    }
+  },
+  methods: {
+    toggleType () {
+      if (this.inputType === 'password') {
+        this.inputType = 'text'
+      } else {
+        this.inputType = 'password'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/master';
+
+input{
+  width: calc(100% - 32px);
+  max-width: 400px;
+  padding: 16px;
+  border-radius: 4px;
+  border: none;
+  background-color: $gray7;
+  outline: none;
+  display: block;
+  font-size: 16px;
+  align-self: center;
+  &::placeholder{
+    color: $gray60;
+    font-size: 15px;
+  }
+}
+
+.password-field{
+  display: flex;
+  justify-content: center;
+  position: relative;
+  left: 12px;
+  .material-icons{
+    align-self: center;
+    position: relative;
+    color: $gray60;
+    left: -40px;
+    cursor: pointer;
+  }
+}
+</style>
