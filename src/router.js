99187957e7d484b846378c2a38d6dbd5c83bf3b7
jarbun <jarbun306@gmail.com>
2019-03-21 10:20:32 +0530

    Added Add page with basic styling

--- src/router.js
+++ src/router.js
@@ -35,6 +35,11 @@ export default new Router({
       path: '/profile',
       name: 'profile',
       component: () => import('./views/Profile.vue')
+    },
+    {
+      path: '/add',
+      name: 'add',
+      component: () => import('./views/Add.vue')
     }
   ]
 })
---
+++ src/views/Add.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <BasicAppBar title="Add"/>
+    <div id="image-field">
+      <button id="image-upload-button">
+        <span class="material-icons">add</span>
+        <span>Add an Image</span>
+      </button>
+    </div>
+    <input type="text" placeholder="Title">
+    <textarea name="" id="" placeholder="Description"></textarea>
+  </div>
+</template>
+
+<script>
+import BasicAppBar from '@/components/BasicAppBar'
+
+export default {
+  name: 'add',
+  components: {
+    BasicAppBar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/master';
+
+#image-field{
+  height: 170px;
+  width: calc(100% - 16px - 16px);
+  margin: 16px 16px 8px 16px;
+  border-radius: 4px;
+  background-color: $gray7;
+  display: flex;
+  justify-content: center;
+}
+
+#image-upload-button{
+  align-self: center;
+  background-color: white;
+  color: $gray60;
+  box-shadow: $e5dp;
+  border-radius: 20px;
+  padding: 8px 16px 8px 8px;
+  .material-icons{
+    padding-right: 8px;
+  }
+  span{
+    vertical-align: middle;
+  }
+}
+
+input{
+  width: calc(100% - 32px);
+  padding: 16px;
+  border-radius: 4px;
+  border: none;
+  background-color: $gray7;
+  outline: none;
+  display: block;
+  margin: 16px auto;
+  font-size: 16px;
+  &::placeholder{
+    color: $gray60;
+    font-size: 15px;
+  }
+}
+
+textarea{
+  width: calc(100% - 32px);
+  height: 110px;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  border-radius: 4px;
+  background-color: $gray7;
+  display: block;
+  margin: 16px auto;
+  outline: none;
+  &::placeholder{
+    color: $gray60;
+    font-size: 15px;
+  }
+}
+</style>
+
