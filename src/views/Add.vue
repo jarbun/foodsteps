516b4de890afe92ce9cadfec7f2c04e6725934ee
jarbun <jarbun306@gmail.com>
2019-03-21 15:07:35 +0530

    Added styling for ingredient addition

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -15,13 +15,31 @@
     <div id="info">
       <div>
         <span>Servings</span>
-        <input type="number" placeholder="02">
+        <input type="text" placeholder="02">
       </div>
       <div>
-        <span>Cook Time</span>
-        <input type="number" placeholder="60">
+        <span>Cook Time (mins)</span>
+        <input type="text" placeholder="60">
       </div>
-    </div> 
+    </div>
+    <div id="buttons">
+      <button :class="{active: activeTab===0}" @click="activeTab = 0">Ingredients</button>
+      <button :class="{active: activeTab===1}" @click="activeTab = 1">Directions</button>
+    </div>
+    <div id="active-component">
+      <div id="ingredients" v-if="activeTab===0">
+        <div id="add-ingredients">
+          <input type="text" placeholder="Ingredient" id="ingredient-name-input">
+          <input type="text" placeholder="Quantity" id="ingredient-quality-input">
+          <div id="add-ingredient-button">
+            <span class="material-icons">add</span>
+          </div>
+        </div>
+      </div>
+      <div id="directions" v-else>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +55,7 @@ export default {
   },
   data () {
     return {
+      activeTab: 0,
       tags: [
         {
           name: 'Vegetarian',
@@ -86,7 +105,7 @@ export default {
   align-self: center;
   background-color: white;
   color: $gray60;
-  box-shadow: $e5dp;
+  box-shadow: $e1dp;
   border-radius: 20px;
   padding: 8px 16px 8px 8px;
   .material-icons{
@@ -144,21 +163,72 @@ textarea{
 #info{
   margin-top: 16px;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
   div{
     display: flex;
     flex-direction: column;
-    width: 100px;
+    width: 160px;
     text-align: center;
     span{
       color: $gray60;
       margin-bottom: 5px;
+      font-size: 15px;
     }
     input{
       width: 60px;
       margin: 0 auto;
       text-align: center;
     }
+  }
+}
+
+#buttons{
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-around;
+  button{
+    width: 100%;
+    color: $gray60;
+    border-bottom: 1px solid $light-gray;
+  }
+  .active{
+    color: $green;
+    border-bottom: 1px solid $green;
+  }
+}
+
+#add-ingredients{
+  display: flex;
+  background-color: $gray7;
+  height: 50px;
+  margin: 10px 16px;
+  input{
+    margin: 0;
+  }
+  border-radius: 25px;
+}
+
+#ingredient-name-input{
+  width: 55%;
+  padding-left: 20px;
+  border-radius: 25px 0 0 25px;
+  border-right: 1px solid $gray50;
+}
+#ingredient-quality-input{
+  width: calc(45% - 49px);
+}
+#add-ingredient-button{
+  width: 48px;
+  height: 48px;
+  box-shadow: $e1dp;
+  align-self: center;
+  background-color: white;
+  border-radius: 50%;
+  text-align: center;
+  cursor: pointer;
+  span{
+    line-height: 48px;
+    color: $gray60;
   }
 }
 </style>
